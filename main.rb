b83b3679270ab704345eb7bd1a9e54015213fec9
jamestucker1988 <xat2212@gmail.com>
2020-10-29 19:45:43 +0530

    define my_each_with_index method

--- main.rb
+++ main.rb
@@ -5,10 +5,18 @@ module Enumerable
 
     for i,k in self
     p.call(i,k)
-     
     end
   end
 
-[1,2,3,4,5].my_each
+# [1,2,3,4,5].my_each {|x| puts x }
+def my_each_with_index()
+  return to_enum(__method__) unless block_given?
+  y=-1
+  self.my_each do|x| 
+   y+=1
+  yield x,y
+end
+end
+#  [1,2,3,4,5].my_each_with_index {|x,y| puts x,y}
 
 end
