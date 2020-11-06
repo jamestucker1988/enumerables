be91fa02696e13501b0700d333fe8903b1d6de04
jamestucker1988 <xat2212@gmail.com>
2020-11-06 20:09:34 +0530

    redefine my_count method

--- main.rb
+++ main.rb
@@ -1,7 +1,6 @@
 module Enumerable
   def my_each(&p)
     return enum_for(__method__) unless block_given?
-
     for  i, k in self
       p.call(i, k)
     end
@@ -60,56 +59,74 @@ module Enumerable
   # my_any?
   def my_any?(arg=nil) 
       if  block_given?
-       my_each {|item| return true if yield(item)==true} 
+        my_each {|item| return true if yield(item)==true} 
       elsif arg.is_a?(Class) 
-        my_each {|item| return true if item.is_a?(arg) }
+           my_each {|item| return true if item.is_a?(arg) }
       elsif arg.is_a?(Regexp)
         my_each {|item| return true if arg.match?(item.to_s)}
       elsif  arg.nil? == false 
         my_each {|item| return true if arg==item}
-      elsif arg.is_a?(Proc)
-        my_each { |item| return true if item }
-      elsif arg.nil? ==true
-         return true
+      else
+        my_each {|item| return true if item }
+      
       end
-     false
+      false
   end
 
+# p [7, 10, 3, 5].my_any?(&:even?) # => true
+# p [7, 10, 4, 5].my_any?(&:even?) # => true
+# p %w[q r s i].my_any? { |char| 'aeiou'.include?(char) } # => true
+# p [7, 11, 3, 5].my_any?(&:even?) # => false
+# p %w[q r s t].my_any? { |char| 'aeiou'.include?(char) } # => false
   
-  # test cases required by tse reviewer
-  # p [3, 5, 4, 11].my_any? # => true
-  # p [1, nil, false].my_any?(1) # => true
-  # p [1, nil, false].my_any?(Integer) # => true
-  # p %w[dog door rod blade].my_any?(/z/) # => false
-  # p [1, 2, 3].my_any?(1) # => true
+# #test cases required by tse reviewer
+# p [3, 5, 4, 11].my_any? # => true
+# p [1, nil, false].my_any?(1) # => true
+# p [1, nil, false].my_any?(Integer) # => true
+# p %w[dog door rod blade].my_any?(/z/) # => false
+# p [1, 2, 3].my_any?(1) # => true
+
+
+
+
+  
   
   # my_none
-  def my_none?(arg=nil)
-    if arg.nil? == true
-    return false 
-    elsif arg.nil? == false
-      my_each {|item| return false if arg == item}
-    end
-    true
+  def my_none?(arg=nil,&proc)
+    !my_any?(arg,&proc)
   end
-  p [1, 2, 3].my_none? # => false
-p [1, 2, 3].my_none?(String) # => true
-p [1, 2, 3, 4, 5].my_none?(2) # => false
-p [1, 2, 3].my_none?(4) # => true
+# p [1, 2, 3].my_none? # => false
+# p [1, 2, 3].my_none?(String) # => true
+# p [1, 2, 3, 4, 5].my_none?(2) # => false
+# p [1, 2, 3].my_none?(4) # => true
+# p [3, 5, 7, 11].my_none?(&:even?) # => true
+# p %w[sushi pizza burrito].my_none? { |word| word[0] == 'a' } # => true
+# p [3, 5, 4, 7, 11].my_none?(&:even?) # => false
+# p %w[asparagus sushi pizza apple burrito].my_none? { |word| word[0] == 'a' } # => false
+def my_count(*args)
+  array = is_a?(Range) ? to_a : self
+  return array.length if !block_given? && args.empty?
 
-  def my_count
-    arr = []
-    my_each do |el|
-      arr << yield(el)
-      return size unless block_given?
-
-      arr << yield(el) if yield(el) == false
-      return arr.size
-
-      ary = [1, 2, 4, 2]
-      puts ary.my_count #=> 4
-      ary.my_count(2) #=> 2
-      ary.my_count { |x| x.even? } #=> 3
+  number = 0
+  if block_given?
+    num_elements = []
+    array.my_each do |item|
+      if yield(item)
+        num_elements << item
+        number = num_elements.length
+      end
     end
+  else
+    array.my_each { |i| number += 1 if i == args[0] } unless args.empty?
   end
+  number
+end
+  p [1, 4, 3, 8].my_count(&:even?) # => 2
+  # p %w[DANIEL JIA KRITI dave].my_count { |s| s == s.upcase } # => 3
+  # p %w[daniel jia kriti dave].my_count { |s| s == s.upcase } # => 0
+  # test cases required by tse reviewer
+  p [1, 2, 3].my_count # => 3
+  p [1, 1, 1, 2, 3].my_count(1) # => 3
+
+
 end
