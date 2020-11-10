cfddd321c7145e81a1256a1124b13d20d9c1d37b
jamestucker1988 <xat2212@gmail.com>
2020-11-10 11:54:59 +0530

    define my_inject method

--- main.rb
+++ main.rb
@@ -48,7 +48,7 @@ module Enumerable
 # p [false, true].my_map(&:!) # => [true, false] 
 # p [1, 2, 3].my_map { |n| 2 * n } # => [2,4,6]
   # my_all
-  def my_all
+  def my_all?(var = nil)
     return to_enum(__method__) unless block_given?
 
     my_each do |e|
@@ -123,7 +123,7 @@ module Enumerable
 #   end
 #   number
 # end
-  def my_count(args=nil,&p)
+def my_count(args=nil,&p)
     count =0 
     if p.is_a?(Proc)
       my_each { |e| count += 1 if p.call(e)}
@@ -148,13 +148,56 @@ module Enumerable
     end
    end
    count
-  end
+end
   
-  p [1, 4, 3, 8].my_count(&:even?) # => 2
-  p %w[daniel jia kriti dave].my_count { |s| s == s.upcase } # => 0
+  # p [1, 4, 3, 8].my_count(&:even?) # => 2
+  # p %w[daniel jia kriti dave].my_count { |s| s == s.upcase } # => 0
   # test cases required by tse reviewer
-  p [1, 2, 3].my_count # => 3
-  p [1, 1, 1, 2, 3].my_count(1) # => 3
+  # p [1, 2, 3].my_count # => 3
+  # p [1, 1, 1, 2, 3].my_count(1) # => 3
 
+  def my_inject(arg1=nil,arg2=nil)
+    if block_given?
+    if arg1.is_a?(Integer)  
+      my_each do |e| 
+     arg1 = yield(arg1,e)
+      end 
+      return  arg1
+    elsif arg1.nil?
+      acc = 0
+      my_each do |e| 
+        acc = yield(acc,e)
+        
+      end
+      return acc
+    end
+    end
+    unless block_given?
+        acc = 0
+        if arg1 == "+" 
+          my_each do |e| 
+            acc += e
+          end
+        return acc
+        end
+    end
+      unless block_given?
+        acc = 1
+        if arg2.to_sym == :*
+          my_each do |e|
+            acc = acc * e
+            
+          end
+        end
+        return acc * arg1
+      end
+     
+  end    
 
+  # p [1, 2, 3, 4].my_inject(10) { |accum, elem|  accum + elem } # => 20
+  # p [1, 2, 3, 4].my_inject { |accum, elem| accum + elem } # => 10
+  # p [5, 1, 2].my_inject('+') # => 8
+  # p (5..10).my_inject(2, :*) # should return 302400
+  # p (5..10).my_inject(4) { |prod, n| prod * n } # should return 604800
+  
 end
