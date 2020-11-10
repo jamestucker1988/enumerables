84a245ed707473bb035d3d179a52a24c0ce6088d
Niko Escobal <niko.escobal@gmail.com>
2020-11-10 17:53:43 +0800

    Delete test cases inside codebase, fix rubocop errors.

--- main.rb
+++ main.rb
@@ -1,15 +1,15 @@
+# frozen_string_literal: true
+
 module Enumerable
+  # my_each
   def my_each(&p)
     return to_enum(__method__) unless block_given?
 
-    for i, k in self
+    each do |i, k|
       p.call(i, k)
     end
   end
 
-  # { 'a' => 1, 'b' => 2 }.my_each { |x, y| puts x, y }
-  #  (1..3).my_each {|x| puts x}
-  # %w(1,2,3,4,5).my_each {|x| puts x}
   # my_each_with_index
   def my_each_with_index
     return to_enum(__method__) unless block_given?
@@ -21,17 +21,15 @@ module Enumerable
     end
   end
 
-  # { 'a' => 1, 'b' => 2 }.my_each_with_index { |x, y| puts x, y }
-  # select
+  # my_select
   def my_select
     arr = []
     my_each { |x| arr << x unless yield(x) == false }
     puts arr
   end
 
-  # [1, 4, 6, 8].my_select { |n| n > 2 }
   # my_map
-  def my_map(proc=nil)
+  def my_map(proc = nil)
     arr = []
     if proc
       my_each { |e| arr << proc.call(e) }
@@ -43,11 +41,6 @@ module Enumerable
     arr
   end
 
-  # [8, 2, 3, 4, 5].my_map { |n| n - 2}
-  #   my_proc = proc { |num| num > 10 }
-  # p [18, 22, 5, 6].my_map(my_proc) { |num| num < 10 }
-  # p [false, true].my_map(&:!) # => [true, false]
-  # p [1, 2, 3].my_map { |n| 2 * n } # => [2,4,6]
   # my_all
   def my_all?(_var = nil)
     return to_enum(__method__) unless block_given?
@@ -58,11 +51,10 @@ module Enumerable
     puts true
   end
 
-  # [1, 2, 3, 4, 5].my_all { |x| x < 7}
   # my_any?
   def my_any?(arg=nil)
     if block_given?
-      my_each { |item| return true if yield(item) == true}
+      my_each { |item| return true if yield(item) == true }
     elsif arg.is_a?(Class)
       my_each { |item| return true if item.is_a?(arg) }
     elsif arg.is_a?(Regexp)
@@ -76,49 +68,11 @@ module Enumerable
     false
   end
 
-  # p [7, 10, 3, 5].my_any?(&:even?) # => true
-  # p [7, 10, 4, 5].my_any?(&:even?) # => true
-  # p %w[q r s i].my_any? { |char| 'aeiou'.include?(char) } # => true
-  # p [7, 11, 3, 5].my_any?(&:even?) # => false
-  # p %w[q r s t].my_any? { |char| 'aeiou'.include?(char) } # => false
-
-  # #test cases required by tse reviewer
-  # p [3, 5, 4, 11].my_any? # => true
-  # p [1, nil, false].my_any?(1) # => true
-  # p [1, nil, false].my_any?(Integer) # => true
-  # p %w[dog door rod blade].my_any?(/z/) # => false
-  # p [1, 2, 3].my_any?(1) # => true
-
   # my_none
   def my_none?(arg=nil, &proc)
     !my_any?(arg, &proc)
   end
-  # p [1, 2, 3].my_none? # => false
-  # p [1, 2, 3].my_none?(String) # => true
-  # p [1, 2, 3, 4, 5].my_none?(2) # => false
-  # p [1, 2, 3].my_none?(4) # => true
-  # p [3, 5, 7, 11].my_none?(&:even?) # => true
-  # p %w[sushi pizza burrito].my_none? { |word| word[0] == 'a' } # => true
-  # p [3, 5, 4, 7, 11].my_none?(&:even?) # => false
-  # p %w[asparagus sushi pizza apple burrito].my_none? { |word| word[0] == 'a' } # => false
-  # def my_count(*args)
-  #   array = is_a?(Range) ? to_a : self
-  #   return array.length if !block_given? && args.empty?
 
-  #   number = 0
-  #   if block_given?
-  #     num_elements = []
-  #     array.my_each do |item|
-  #       if yield(item)
-  #         num_elements << item
-  #         number = num_elements.length
-  #       end
-  #     end
-  #   else
-  #     array.my_each { |i| number += 1 if i == args[0] } unless args.empty?
-  #   end
-  #   number
-  # end
   def my_count(args=nil, &p)
     count = 0
     if p.is_a?(Proc)
@@ -146,12 +100,7 @@ module Enumerable
     count
   end
 
-  # p [1, 4, 3, 8].my_count(&:even?) # => 2
-  # p %w[daniel jia kriti dave].my_count { |s| s == s.upcase } # => 0
-  # test cases required by tse reviewer
-  # p [1, 2, 3].my_count # => 3
-  # p [1, 1, 1, 2, 3].my_count(1) # => 3
-
+  # my_inject
   def my_inject(arg1=nil, arg2=nil)
     if block_given?
       if arg1.is_a?(Integer)
@@ -191,14 +140,9 @@ module Enumerable
       acc
     end
   end
-
-  p [1, 2, 3, 4].my_inject(10) { |accum, elem| accum + elem } # => 20
-  p [1, 2, 3, 4].my_inject { |accum, elem| accum + elem } # => 10
-  p [5, 1, 2].my_inject('+') # => 8
-  p (5..10).my_inject(2, :*) # should return 302400
-  p(5..10).my_inject(4) { |prod, n| prod * n } # should return 604800
 end
 
+# multiply_els
 def multiply_els(arg)
   arg.my_inject(1) { |r, x| r * x}
 end
